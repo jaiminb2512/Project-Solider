bcb5edc0308fe96785bc1e46b1dac864b67338cf
jaiminb2512 <137876433+jaiminb2512@users.noreply.github.com>
2024-07-15 21:08:49 +0530

    Add files via upload

---
+++ React/Website/Health+/README.md
@@ -0,0 +1,5 @@
+Make two folder named Assets in src 
+
+In Asssests store phots
+
+Complete the reviews.js's array
---
+++ React/Website/Health+/src/Scripts/reviews.js
@@ -0,0 +1,52 @@
+export const customerReviews = [
+  {
+    "name": "",
+    "location": "",
+    "message": ""
+  },
+  {
+    "name": "",
+    "location": "",
+    "message": ""
+  },
+  {
+    "name": "",
+    "location": "",
+    "message": ""
+  },
+  {
+    "name": "",
+    "location": "",
+    "message": ""
+  },
+  {
+    "name": "",
+    "location": "",
+    "message": ""
+  },
+  {
+    "name": "",
+    "location": "",
+    "message": ""
+  },
+  {
+    "name": "",
+    "location": "",
+    "message": ""
+  },
+  {
+    "name": "",
+    "location": "",
+    "message": ""
+  },
+  {
+    "name": "",
+    "location": "",
+    "message": ""
+  },
+  {
+    "name": "",
+    "location": "",
+    "message": ""
+  }
+];
